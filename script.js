9ee882b28b3c88b32e3cf720933a05ccdd53a999
Ansh Saini <108681543+ProblematicPP@users.noreply.github.com>
2025-06-11 22:39:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.getElementById('yesBtn').addEventListener('click', () => {
+//   alert('💩 Confirmed.');
+// });
+
+// document.getElementById('noBtn').addEventListener('click', () => {
+//   alert('Suspicious...');
+// });
